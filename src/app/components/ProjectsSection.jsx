593c8a67384d9066a8ad6da83c03130b52e49df8
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-05-08 08:57:54 +0530

    Update project Section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,58 +7,60 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
+    title: "HackNITR 5.0 Website",
+    description:
+      "HackNITR 5.0: Flagship Hackathon to solve real-world challenges by GDSC NIT Rourkela and OpenCode.",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/dscnitrourkela/project-oregano",
+    previewUrl: "https://hacknitr.com",
   },
   {
     id: 2,
-    title: "Potography Portfolio Website",
-    description: "Project 2 description",
+    title: "Coratia Website",
+    description: "Website of Shark Tank India featured company",
     image: "/images/projects/2.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/Seventy7dot2/project-coratia",
+    previewUrl: "https://coratia.com",
   },
   {
     id: 3,
     title: "E-commerce Application",
-    description: "Project 3 description",
+    description:
+      "The Grocery Store App is a user-friendly and efficient web application designed to streamline your grocery shopping experience",
     image: "/images/projects/3.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/Seventy7dot2/Grocery_Store",
+    previewUrl: "https://www.youtube.com/watch?v=YHBXEdMetbA",
   },
   {
     id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
+    title: "Boost: Motivational WhatsApp Bot",
+    description: "A Whatsapp bot which sends motivational messages.",
     image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
+    tag: ["All"],
+    gitUrl: "https://github.com/Seventy7dot2/whatsappbot",
+    previewUrl: "https://devfolio.co/projects/boost-e7ef",
   },
   {
     id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
+    title: "IIT Madras Grade Predictor",
+    description: "Grade Predictor for IITM BS students",
     image: "/images/projects/5.png",
     tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
+    gitUrl: "https://github.com/Seventy7dot2/iitm_calculator",
+    previewUrl: "https://seventy7dot2.github.io/iitm_calculator/",
   },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
+  // {
+  //   id: 6,
+  //   title: "Full-stack Roadmap",
+  //   description: "Project 5 description",
+  //   image: "/images/projects/6.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "/",
+  //   previewUrl: "/",
+  // },
 ];
 
 const ProjectsSection = () => {
@@ -95,11 +97,11 @@ const ProjectsSection = () => {
           name="Web"
           isSelected={tag === "Web"}
         />
-        <ProjectTag
+        {/* <ProjectTag
           onClick={handleTagChange}
-          name="Mobile"
-          isSelected={tag === "Mobile"}
-        />
+          name="ML"
+          isSelected={tag === "ML"}
+        /> */}
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
